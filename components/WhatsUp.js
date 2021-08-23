54ec683b5d759f92a308f15fcf9bcfa87b5ed57f
Nick G <nicholasgalante1997@gmail.com>
2021-08-23 10:32:56 -0600

    cosmetic patches for responsiveness

--- components/WhatsUp.js
+++ components/WhatsUp.js
@@ -1,12 +1,12 @@
 import React from "react";
 import userData from "@constants/data";
 
-export default function LatestCode() {
+export default function WhatsUp() {
   return (
-    <section className="bg-[#F1F1F1] -mt-60 dark:bg-gray-900 py-20">
+    <section className="bg-[#F1F1F1] -mt-50 md:-mt-60 dark:bg-gray-900 py-20">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
-          <h4 className="text-5xl lg:text-5xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center lg:text-left">
+          <h4 className=" text-5xl lg:text-5xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center lg:text-left">
             A What's Up To Arms
           </h4>
         </div>
